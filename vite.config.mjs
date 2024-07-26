05fc8422aa20a275c9239f0ea9e5cdb6b4c58bac
Artem Korolev <artkor@live.ru>
2024-07-26 04:04:46 +0300

    configure Vite with base url

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,9 +3,11 @@ import { resolve, dirname } from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+import CONFIG from "src/config"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: CONFIG.VITE_BASE_URL,
   plugins: [
     vue(),
     VueI18nPlugin({
