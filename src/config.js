673ee6ae2ca31e890cee125fd9d8e103f66a54c6
Artem Korolev <artkor@live.ru>
2024-07-28 13:17:33 +0300

    adding Icelandic language

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ export default {
   VITE_BASE_URL: '/drugncoin/',
   VITE_DEFAULT_LOCALE: 'en',
   VITE_FALLBACK_LOCALE: 'en',
-  VITE_SUPPORTED_LOCALES: 'bg,cs,de,en,es,et,fa,fi,fr,he,it,lt,lv,no,pl,pt,ru,sv,ua'
+  VITE_SUPPORTED_LOCALES: 'bg,cs,de,en,es,et,fa,fi,fr,he,is,it,lt,lv,no,pl,pt,ru,sv,ua'
 }
