1e755ca5c55cdab02659bc8ff7853ec31e83b7ef
Artem <artem-korolev@users.noreply.github.com>
2024-08-01 13:12:44 +0300

    moving to custom domain - drugncoin.com - with application in the root /

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  VITE_BASE_URL: '/drugncoin/',
+  VITE_BASE_URL: '/',
   VITE_DEFAULT_LOCALE: 'en',
   VITE_FALLBACK_LOCALE: 'en',
   VITE_SUPPORTED_LOCALES: 'bg,cs,de,en,es,et,fa,fi,fr,he,is,it,lt,lv,no,pl,pt,ru,sv,ua'
